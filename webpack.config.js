4d7ac0bb279cfa349456f571042600951b7f50c7
Ryan Mattox <509528@gmail.com>
2020-07-17 15:13:00 -0700

    Installed scss and added color

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 var path = require("path");
+var postcssPresetEnv = require("postcss-preset-env");
 
 
 module.exports = {
@@ -14,10 +15,32 @@ module.exports = {
                 }
             },
             {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-
-            }
+                // For pure CSS - /\.css$/i,
+                // For Sass/SCSS - /\.((c|sa|sc)ss)$/i,
+                // For Less - /\.((c|le)ss)$/i,
+                test: /\.((c|sa|sc)ss)$/i,
+                use: [
+                'style-loader',
+                {
+                    loader: 'css-loader',
+                    options: {
+                    // Run `postcss-loader` on each CSS `@import`, do not forget that `sass-loader` compile non CSS `@import`'s into a single file
+                    // If you need run `sass-loader` and `postcss-loader` on each CSS `@import` please set it to `2`
+                    importLoaders: 2,
+                    // Automatically enable css modules for files satisfying `/\.module\.\w+$/i` RegExp.
+                    modules: { auto: true },
+                    },
+                },
+                {
+                    loader: 'postcss-loader',
+                    options: { plugins: () => [postcssPresetEnv({ stage: 0 })] },
+                },
+                // Can be `less-loader`
+                {
+                    loader: 'sass-loader',
+                },
+                ],
+            },
 
         ]
     },
